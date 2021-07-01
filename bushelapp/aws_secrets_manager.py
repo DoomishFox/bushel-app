f527c4daa50180f4b2d2c23a435e278b1e569c54
Chutka <chutkasc@88665a0f28ce.ant.amazon.com>
2021-07-01 15:54:13 -0400

    removed python venv from git

--- bushelapp/aws_secrets_manager.py
+++ bushelapp/aws_secrets_manager.py
@@ -10,16 +10,12 @@ from botocore.exceptions import ClientError
 
 
 def get_secret(secret, environment):
-    aws_access_key_id = os.environ['AWS_ACCESS_KEY']
-    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS']
 
     secret_name = "bushel/" + environment + "/" + secret
     region_name = "us-east-1"
 
     # Create a Secrets Manager client
-    session = boto3.session.Session(aws_access_key_id=aws_access_key_id,
-                                    aws_secret_access_key=aws_secret_access_key,
-                                    region_name=region_name)
+    session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
